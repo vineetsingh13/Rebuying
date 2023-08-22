d78b274fdc4e6cc813fee0751b2c087915a06a7e
Vineet Singh <singhvineet13502@gmail.com>
2023-08-22 21:22:45 +0530

    created the chat activity nothing else and added the onResume method in the home fragment to clear the error in the bottom navigation view item selected listener

--- app/src/main/java/com/example/rebuying/HomeActivity.kt
+++ app/src/main/java/com/example/rebuying/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.example.rebuying
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.rebuying.ChatFragment.ChatActivity
 import com.example.rebuying.HomeFragment.HomeFragment
 import com.example.rebuying.databinding.ActivityHomeBinding
 
@@ -21,12 +23,21 @@ class HomeActivity : AppCompatActivity() {
 
                 R.id.bottom_nav_home->{
                     replaceFragment(HomeFragment())
+
+                }
+                R.id.bottom_nav_chat->{
+                    val i=Intent(this@HomeActivity,ChatActivity::class.java)
+                    startActivity(i)
                 }
             }
             true
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        binding.bottomNavigationView.menu.findItem(R.id.bottom_nav_home).isChecked=true
+    }
     private fun replaceFragment(fragment: Fragment){
 
         val fragmentManager=supportFragmentManager
